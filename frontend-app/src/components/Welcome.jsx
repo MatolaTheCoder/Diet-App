4d627e69e51f2f0a25cb15e098650d5441e5d5a3
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-09-24 18:49:56 +0200

    feat: removi o codigo do header e chamei como componente style: animacao no texto da pagina

--- frontend-app/src/components/Welcome.jsx
+++ frontend-app/src/components/Welcome.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
+import InicialHeader from './reutilizaveis/InicialHeader'
 
 export default function Welcome() {
     return (
         <div className='bg-emerald-50 h-svh'>
             <div className="">
-                <nav class="w-full bg-white dark:bg-slate-900">
-                    <div class="container mx-auto py-4 flex items-center justify-between">
-                        <div class="flex flex-col items-center p-4">
-                                <span class="font-nunito text-green-400">Diet-<span className='text-gray-400'>App</span></span>
-                        </div>
-                        <div>
-                            <ul class="hidden md:flex gap-8  font-semibold text-sm text-gray-600 underline">
-                                <li class="hover:text-gray-800">Registe-se</li>
-                                <li class="hover:text-gray-800 pr-8">Login</li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                <InicialHeader/>
             </div>
             <div className="flex justify-center">
                 <div className="pt-24 pr-8">
