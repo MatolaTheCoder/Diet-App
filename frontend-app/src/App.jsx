d569501055fe91ee9a9759ef1a484433484bc2cb
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-09-27 23:57:38 +0200

    feat: rota da pagina de actualizar dados

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -4,6 +4,7 @@ import {Routes, Route} from 'react-router-dom'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Home from './components/Home'
+import Update from './components/auth/Update'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Welcome/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/update' element={<Update/>}/>
         <Route path='/home' element={<Home/>}/>
       </Routes>
     </>
