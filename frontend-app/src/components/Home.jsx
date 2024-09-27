5bd0114fac76abec559641889828369259200071
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-09-27 22:58:11 +0200

    feat: pagina home

--- frontend-app/src/components/Home.jsx
+++ frontend-app/src/components/Home.jsx
@@ -1,52 +1,41 @@
-import React from 'react'
-import InicialHeader from './reutilizaveis/InicialHeader'
-import InputField from './reutilizaveis/InputField'
+import React from 'react';
 
 export default function Home() {
+    const dados = 'Dieta';
+    const orientacao = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam autem laborum repellat, eius a illum debitis et numquam cupiditate quam cumque, delectus excepturi ut placeat dolore, nemo quae quaerat quis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, impedit nesciunt! Ipsum ipsam eos corporis molestiae, quam voluptatem earum minus quod amet, veritatis aut quasi rerum alias excepturi quis eum.';
+
     return (
         <div>
             <div className="">
-            <nav class="w-full bg-white shadow">
-                <div class="container max-w-7xl mx-auto py-4 flex items-center justify-between">
-                    <div class="flex flex-col items-center py-4">
-                        <span class="font-nunito text-green-400 "><span className='text-xl font-medium'>Diet</span>-<span className='text-gray-400 text-lg'>App</span></span>
+                <nav className="w-full bg-white shadow">
+                    <div className="container max-w-7xl mx-auto py-4 flex items-center justify-between">
+                        <div className="flex flex-col items-center py-4">
+                            <span className="font-nunito text-green-400">
+                                <span className='text-xl font-medium'>Diet</span>-<span className='text-gray-400 text-lg'>App</span>
+                            </span>
+                        </div>
+                        <div>
+                            <p className="text-emerald-500 ">Bem-Vindo</p>
+                        </div>
                     </div>
-                    <div>
-                        <p className="text-emerald-500 ">Bem-Vindo</p>
-                    </div>
-                </div>
-            </nav>
+                </nav>
             </div>
-            <div className="bg-neutral-50 w-full h-full">
+            <div className="bg-neutral-50 w-full min-h-screen">
                 <div className="mt-3 max-w-7xl mx-auto font-semibold text-2xl p-6 text-gray-600">
                     | Pagina Inicial
                 </div>
                 <div className="mt-10">
-                    <div className="bg-white max-w-7xl mx-auto p-5 shadow rounded-r-lg">
-                        <form action="">
+                    <div className=" max-w-5xl mx-auto p-5 rounded-lg">
                             <div className="w-full flex flex-col justify-center ">
-                                <h2 className="text-2xl text-center text-gray-600 font-semibold mb-6">Dados</h2>
-                                <div className="flex justify-center flex-row gap-8">
-                                    <div className="">
-                                        <InputField label={'Idade'} name={'idade'} onChange={''} />
-                                        <InputField label={'Peso'} name={'peso'}  onChange={''} />
-                                        <InputField label={'Altura'} name={'altura'} onChange={''} />
-                                    </div>
-                                    <div className="">
-                                        <InputField label={'Objectivos'} name={'objectivos'} onChange={''} />
-                                        <InputField label={'Restricoes'} name={'restricoes'} onChange={''} />
-                                    </div>
-                                </div>
-                                <div className="mt-6 flex justify-center">
-                                    <button className="bg-emerald-300 text-gray-600 px-6 py-2 rounded-full font-semibold hover:bg-emerald-400">
-                                        Registar
-                                    </button>
+                                <h2 className="text-2xl text-center text-gray-700 font-semibold mb-6">{dados}</h2>
+                                <div className="bg-gray-50 p-6 shadow-md rounded-md mb-6">
+                                    <h3 className="text-xl text-gray-700 font-semibold mb-3">Sobre a {dados}</h3>
+                                    <p className="text-gray-500">{orientacao}</p>
                                 </div>
                             </div>
-                        </form>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
